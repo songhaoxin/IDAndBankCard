41bd11c9a0b027036a5f21b44f747619fd91ed81
Hop Ma <13027768987@163.com>
2016-12-22 17:21:24 +0800

    first commit

---
+++ IDAndBankCard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IDAndBankCard
+//
+//  Created by mxl on 2016/12/22.
+//  Copyright © 2016年 Meloinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
